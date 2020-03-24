96175de1253cdff78e46be0c9f07981f05c80633
boringtao <xinghui.tao@wecorpindustries.com>
2020-03-24 15:05:18 +0000

    Add: evn_python3.sh

--- env_python3.sh
+++ env_python3.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-sudo apt-get install python-catkin-tools python3-dev python3-numpy
+# Install pip3
+cd ~/Installers
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 rm -f get-pip.py
-sudo pip3 install virtualenv virtualenvwrapper
+cd ~
 
+sudo apt-get install python-catkin-tools python3-dev python3-numpy python3-empy
+sudo pip3 install virtualenv
+
+# Set up py3venv within Workspace
+cd ~/Workspace
 mkdir -p py3venv/src
 cd py3venv
-virtualenv python3 --python=python3
+virtualenv py3 --python=python3
 source python3/bin/activate
 
-cd src
+pip3 install pyaml rospkg empy
+
+# (Optional) Install baselines for reinforcement learning
 pip3 install tensorflow==1.15rc2
+cd src
+git clone https://github.com/openai/baselines.git
 cd baselines
 pip3 install -e .
 pip3 install gym
+cd ..
 
-git clone https://github.com/ros/geometry
-git clone https://github.com/ros/geometry2
-pip3 install pyaml rospkg
-pip3 uninstall empy
-pip3 install empy 
+# Catkin make
+catkin_make -DPYTHON_EXECUTABLE:FILEPATH=~/Workspace/py3venv/py3/bin/python
+source devel/setup.bash
